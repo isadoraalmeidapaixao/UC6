6fd304301cefd051b48416cfdc65eb5dc4335cf1
isadora <isadoraalmeidapaixao@gmail.com>
2025-11-19 22:02:08 -0300

    Delete projetos/EstacionamentoDesktop directory

--- projetos/EstacionamentoDesktop/Estacionamento.MAUI/Models/Vaga.cs
+++
@@ -1,9 +0,0 @@
-namespace Estacionamento.MAUI.Models
-{
-    public class Vaga
-    {
-        public int Id { get; set; }
-        public string Localizacao { get; set; } = string.Empty;
-        public string? TipoVaga { get; set; }
-    }
-}
